331023c350e41f9f2be3c833ab6e6a150b1bd99c
specialk731 <kevingreenwald13@gmail.com>
2016-03-23 10:33:13 -0500

    Initial Commit

---
+++ TaskExecutorStudentDevProject/src/edu/utdallas/taskExecutorImpl/TaskExecutorImpl.java
@@ -0,0 +1,15 @@
+package edu.utdallas.taskExecutorImpl;
+
+import edu.utdallas.taskExecutor.Task;
+import edu.utdallas.taskExecutor.TaskExecutor;
+
+public class TaskExecutorImpl implements TaskExecutor
+{
+
+	@Override
+	public void addTask(Task task)
+	{
+		// TODO Complete the implementation
+	}
+
+}
